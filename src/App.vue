539af20f1eb0ec84473dd5aa346a28708be0018b
Alvarows20 <alvarojpt04.ws@gmail.com>
2024-09-10 15:02:00 -0500

    Adding packages, dependencies and project files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+export default defineComponent({
+  setup() {
+    const { result, loading, error } = useQuery(gql`
+      query {
+        continents {
+          countries {
+            name
+            capital
+            currency
+          }
+        }
+      }
+    `);
+
+    return {
+      result,
+      loading,
+      error,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp, h, provide } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+const httpLink = createHttpLink({
+    uri: 'https://countries.trevorblades.com/' // // URI del servidor GraphQL
+})
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+    link: httpLink, cache})
+
+createApp({ 
+    setup(){
+        provide(DefaultApolloClient, apolloClient);
+    },
+    render:()=>
+        h(App),
+
+}).mount('#app')
